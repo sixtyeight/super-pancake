d9faec009a3f0ccf13ea2d9e7863386c8e7a6b98
sixtyeight <christian_sl@gmx.at>
2018-05-08 20:23:19 +0200

    initial import

---
+++ src/main/java/superpancake/contentdetectorservice/ContentdetectorServiceApplication.java
@@ -0,0 +1,12 @@
+package superpancake.contentdetectorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentdetectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentdetectorServiceApplication.class, args);
+	}
+}
